1b5944136d2660a723264c42e36a59e6fee13705
Diego <diegopigola06@gmail.com>
2021-11-08 21:59:56 -0300

    Con esto hardcode me anduvo

--- src/services/search.js
+++ src/services/search.js
@@ -1,15 +1,21 @@
 import axios from "axios";
 
-export function NewSearch(url_params, body) {
+export function NewSearch(input, body) {
+  
+  const url_params = "alq-20000/_search"
   const request = {
       url: url_params,
       method: "post",
       baseURL: "http://localhost:9200",
       body: body,
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': 'http://localhost:3000',
+        'Access-Control-Allow-Methods':'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+        'Access-Control-Allow-Headers': 'X-Requested-With, Content-Length, Content-Type, Authorization, Accept',
+        'Access-Control-Allow-Credentials': true,
+        'X-Requested-With': 'XMLHttpRequest',
+        'Content-Type': 'application/json'
+
       }
   };
   console.log(request);
